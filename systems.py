23701afedd51f64225fa0c86fdc9e84ebf997136
henriquexbrito2 <canalhxbproducoes@gmail.com>
2026-06-30 23:41:31 -0300

    Create systems.py

---
+++ systems.py
@@ -0,0 +1,7 @@
+import random
+
+def generate_xp(min_xp, max_xp):
+    return random.randint(min_xp, max_xp)
+
+def generate_work_reward(min_money, max_money):
+    return random.randint(min_money, max_money)
