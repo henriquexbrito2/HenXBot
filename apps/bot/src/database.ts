db159c3ab112d6db423c04912ea2b316de6130fd
henriquexbrito2 <canalhxbproducoes@gmail.com>
2026-07-15 19:11:06 -0300

    Create database.ts

---
+++ apps/bot/src/database.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export async function connectDatabase() {
+try {
+
+    await mongoose.connect(process.env.MONGO_URI!);
+
+    console.log("✅ MongoDB conectado");
+
+} catch (error) {
+
+    console.error("❌ MongoDB:", error);
+
+}
+
+}
